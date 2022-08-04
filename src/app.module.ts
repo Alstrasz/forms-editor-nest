0f96eb94bdeacd385ead9ca92687f497404663fe
Alstrasz <lutsai2000@ya.ru>
2022-08-04 16:53:30 +0300

    heroku postgres db fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,22 +9,40 @@ import { JsonRpcModule } from './json_rpc/json_rpc.module';
 @Module( {
     imports: [
         FormsModule,
-        TypeOrmModule.forRoot( {
-            type: 'postgres',
-            host: process.env.POSTGRES_HOST || 'localhost',
-            port: parseInt( process.env.POSTGRES_PORT || '5432' ),
-            username: process.env.POSTGRES_USER || 'root',
-            password: process.env.POSTGRES_PASSWORD || 'root',
-            database: process.env.POSTGRES_DB || 'test',
-            synchronize: true,
-            autoLoadEntities: true,
-            ssl: process.env.POSTGRES_USE_SSL !== undefined,
-            extra: process.env.POSTGRES_USE_SSL === undefined ? undefined : {
-                ssl: {
-                    rejectUnauthorized: false,
-                },
-            },
-        } ),
+        TypeOrmModule.forRoot(
+            process.env.DATABASE_URL ?
+                {
+                    type: 'postgres',
+
+                    url: process.env.DATABASE_URL,
+
+                    synchronize: true,
+                    autoLoadEntities: true,
+                    ssl: process.env.POSTGRES_USE_SSL !== undefined,
+                    extra: process.env.POSTGRES_USE_SSL === undefined ? undefined : {
+                        ssl: {
+                            rejectUnauthorized: false,
+                        },
+                    },
+                } :
+                {
+                    type: 'postgres',
+
+                    host: process.env.POSTGRES_HOST || 'localhost',
+                    port: parseInt( process.env.POSTGRES_PORT || '5432' ),
+                    username: process.env.POSTGRES_USER || 'root',
+                    password: process.env.POSTGRES_PASSWORD || 'root',
+                    database: process.env.POSTGRES_DB || 'test',
+
+                    synchronize: true,
+                    autoLoadEntities: true,
+                    ssl: process.env.POSTGRES_USE_SSL !== undefined,
+                    extra: process.env.POSTGRES_USE_SSL === undefined ? undefined : {
+                        ssl: {
+                            rejectUnauthorized: false,
+                        },
+                    },
+                } ),
         ResponsesModule,
         JsonRpcModule,
     ],
